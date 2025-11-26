fd7e412dc8da584d7b7e5778b99835d666beb8fd
IsaacMorzy <musyokaisaac98@gmail.com>
2025-11-25 18:42:20 -0600

    chore: bootstrap Lingua app via bench new-app

---
+++ lingua/lingua/doctype/lingua_language/lingua_language.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, IsaacMorzy and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Lingua Language", {
+// 	refresh(frm) {
+
+// 	},
+// });
