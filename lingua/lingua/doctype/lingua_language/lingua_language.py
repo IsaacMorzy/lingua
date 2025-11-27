583601432a7a943c079ced6be1a929c091cd8c20
IsaacMorzy <musyokaisaac98@gmail.com>
2025-11-27 14:34:27 +0000

    feat: core Lingua level system and language model with tests

--- lingua/lingua/doctype/lingua_language/lingua_language.py
+++ lingua/lingua/doctype/lingua_language/lingua_language.py
@@ -1,9 +1,62 @@
-# Copyright (c) 2025, IsaacMorzy and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class LinguaLanguage(Document):
-	pass
+	def validate(self):
+		self._normalize_code()
+		self._ensure_unique_code()
+		self._validate_default_level_system()
+
+	def _normalize_code(self):
+		"""Keep language codes short, uppercase, and trimmed."""
+		if self.code:
+			self.code = self.code.strip().upper()
+
+	def _ensure_unique_code(self):
+		"""Enforce unique language code across all languages."""
+		if not self.code:
+			return
+
+		# For NEW docs, we just check by code.
+		# For existing docs, exclude self by name.
+		filters = {"code": self.code}
+		if not self.is_new():
+			filters["name"] = ["!=", self.name]
+
+		existing = frappe.db.get_all(
+			"Lingua Language",
+			filters=filters,
+			pluck="name",
+			limit=1,
+		)
+
+		if existing:
+			frappe.throw(
+				_("Language code {0} is already in use.").format(self.code),
+				frappe.ValidationError,
+			)
+
+	def _validate_default_level_system(self):
+		"""If a default level system is set, ensure it exists and is active."""
+		if not self.default_level_system:
+			return
+
+		active = frappe.db.get_value(
+			"Lingua Level System",
+			self.default_level_system,
+			"is_active",
+		)
+
+		if active is None:
+			frappe.throw(
+				_("Default Level System {0} does not exist.").format(self.default_level_system),
+				frappe.ValidationError,
+			)
+
+		if not active:
+			frappe.throw(
+				_("Default Level System {0} is not active.").format(self.default_level_system),
+				frappe.ValidationError,
+			)
--- lingua/lingua/doctype/lingua_level_system/lingua_level_system.py
+++ lingua/lingua/doctype/lingua_level_system/lingua_level_system.py
@@ -1,9 +1,51 @@
-# Copyright (c) 2025, IsaacMorzy and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
+from frappe import ValidationError, _
 from frappe.model.document import Document
 
 
 class LinguaLevelSystem(Document):
-	pass
+	def validate(self):
+		self._normalize_code()
+		self._ensure_unique_code()
+		self._ensure_single_default()
+
+	def _normalize_code(self):
+		"""Ensure code is uppercase and trimmed."""
+		if self.code:
+			self.code = self.code.strip().upper()
+
+	def _ensure_unique_code(self):
+		"""Code must be unique across all level systems."""
+		if not self.code:
+			return
+
+		filters = {"code": self.code}
+		if not self.is_new():
+			filters["name"] = ["!=", self.name]
+
+		existing = frappe.db.get_all(
+			"Lingua Level System",
+			filters=filters,
+			pluck="name",
+			limit=1,
+		)
+
+		if existing:
+			frappe.throw(
+				_("Code {0} is already used by another Level System.").format(self.code),
+				frappe.ValidationError,
+			)
+
+	def _ensure_single_default(self):
+		"""Only one system can be marked as default."""
+		if not self.is_default:
+			return
+
+		frappe.db.sql(
+			"""
+            UPDATE `tabLingua Level System`
+            SET is_default = 0
+            WHERE name != %s
+            """,
+			(self.name or "New Lingua Level System",),
+		)
